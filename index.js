b4943bef550f096b7c46cb73073860dd202dd631
unknown <piyush222rana@gmail.com>
2023-02-04 15:35:50 +0530

    Output change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3030;
 app.get('/', async(req,res) => {
     const result = await axios.get('https://superheroapi.com/api/accesscode/1')
     console.log(result.data.name);
-    res.send(result.data.name);
+    res.send("Hey");
 });
 
 app.listen(PORT, () => {
